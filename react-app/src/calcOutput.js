5fa52fbb2e343e3d8cd613fa7b174be7f50b951f
clarkm18 <33027383+clarkm18@users.noreply.github.com>
2020-10-06 18:58:50 +0100

    Current issue: files not importing

---
+++ react-app/src/calcOutput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Component} from 'react';
+
+class calcOutput extends Component {
+    render() {
+        let {result} = this.props;
+        return( 
+            <div className="result">
+                <p>{result}</p>
+            </div>
+        )
+    }
+}
+
+export default calcOutput;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,73 +1,116 @@
 import React from 'react';
+import {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import calcLayout from "./calcLayout.js";
+import calcOutput from "./calcOutput.js";
 
-function App() {
-  return (
-    <BrowserRouter>
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+          <p>
+            Welcome to the incomplete app!
+          </p>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/calculator">Calculator</Link>
+              </li>
+            </ul>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/calculator">
+                <Calculator />
+              </Route>
+            </Switch>
+          </header>
+        </div>
+      </BrowserRouter>
+    );
+  }  
+}
+
+class Home extends Component {
+  render() {
+    return (
       <div className="App">
         <header className="App-header">
-        <p>
-          Welcome to the incomplete app!
-        </p>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/calculator">Calculator</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/calculator">
-              <Calculator />
-            </Route>
-          </Switch>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/matthew-clark-15284517b/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Matthew's LinkedIn
+          </a>
+          <a
+            className="App-link"
+            href="http://www.semantic-web-journal.net/system/files/swj2493.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Matthew's Technical Resource
+          </a>
         </header>
       </div>
-    </BrowserRouter>
-  );
+    );
+  }
 }
 
-function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/matthew-clark-15284517b/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Matthew's LinkedIn
-        </a>
-        <a
-          className="App-link"
-          href="http://www.semantic-web-journal.net/system/files/swj2493.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Matthew's Technical Resource
-        </a>
-      </header>
-    </div>
-  );
-}
+class Calculator extends Component {
+  constructor(){
+    super();
+    this.state = {
+      result: ""
+    }
+  }
 
-function Calculator() {
-  var calculatorElement = 1;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{calculatorElement + calculatorElement}</h1>
-      </header>
-    </div>
-    )
+  calcFunc = () => {
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+      })
+    } catch(e){
+      this.setState({
+        result: "ERROR"
+      })
+    }
+  };
+
+  resetFunc = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  onClick = button => {
+    if(button === "="){
+      this.calcFunc()
+    }
+
+    if(button === "C"){
+      this.resetFunc()
+    }
+  }
+
+  render() {
+    return (
+      <div className="calcLayout">
+        <h1>Calculator Program</h1>
+        <calcOutput result={this.state.result}/>
+        <calcLayout onClick={this.onClick}/>
+      </div>
+      )
+  }
 }
 
 ReactDOM.render(
