e078c3ce06fffdfe66956d2a111168d66c317cd8
clarkm18 <33027383+clarkm18@users.noreply.github.com>
2020-10-06 17:47:22 +0100

    Added react-router-dom and first 2 links

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders LinkedIn link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Matthew's LinkedIn/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+//This is an incomplete test suite, come back to this once app development is finished
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+        <p>
+          Welcome to the incomplete app!
+        </p>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Calculator</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/calculator">
+              <Calculator />
+            </Route>
+          </Switch>
+        </header>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/matthew-clark-15284517b/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Matthew's LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href="http://www.semantic-web-journal.net/system/files/swj2493.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Matthew's Technical Resource
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function Calculator() {
+  var calculatorElement = 1;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{calculatorElement + calculatorElement}</h1>
+      </header>
+    </div>
+    )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
